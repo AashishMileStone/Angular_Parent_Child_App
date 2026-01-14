71ea953cee6e2e7f2710e44edac3e25340da4236
Aashish Kumar <aashish.p@droisys.com>
2026-01-14 17:25:21 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet} from '@angular/router';
+import { ParentComponent } from './parent/parent.component';
+import { CheckBioComponent } from './check-bio/check-bio.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ParentComponent, CheckBioComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angularApp';
+
+  
+  user = {"name": "Admin", "isAdmin": false}
+  isValid = false;
+  admin(){
+    this.user.isAdmin = true;
+    this.isValid = true;
+  }
+  userLog(){
+    this.user.isAdmin = false;
+    this.isValid = true;
+  }
 }
